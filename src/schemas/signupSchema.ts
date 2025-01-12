158210f6a6016f07c45fc45bf41de1391034ef52
xensen008 <arnabjyotikakati008@gmail.com>
2025-01-13 04:18:24 +0530

    Created sign-up page, update username validation message

--- src/schemas/signupSchema.ts
+++ src/schemas/signupSchema.ts
@@ -1,12 +1,12 @@
 import {z} from 'zod';
 
-export  const  usernameValidation =  z
+export const usernameValidation = z
     .string()
     .min(3, 'Username must be at least 3 characters long')
     .max(20, 'Username must be at most 20 characters long')
-    .regex(/^[a-zA-Z0-9_]+$/, 'Username must not have special charaters');
+    .regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores');
 
-
+    
 export const signupSchema = z.object({
     username : usernameValidation,
     email : z.string().email({
