65508b02bedb69b00093ffada61ee819ab17bbed
arnabjyotikakati008 <arnabjyotikakati008@gmail.com>
2025-01-16 20:15:49 +0000

    simple bug fix (api spelled wrong)

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -33,7 +33,7 @@ export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
   const handleDeleteConfirm = async () => {
     try {
       const response = await axios.delete<ApiResponse>(
-        `/api/delete-message/${message._id}`
+        `/api/delete-messages/${message._id}`
       );
       toast({
         title: response.data.message,
