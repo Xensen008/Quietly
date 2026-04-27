f4bef50e9e0cccb9786dbc6144e1bc2cace30d8f
xensen008 <arnabjyotikakati008@gmail.com>
2026-04-27 12:00:00 +0530

    chore: update

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -29,7 +29,7 @@ type MessageCardProps = {
 };
 
 
-//change
+//TODO:change
 export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
   const { toast } = useToast();
   const messageCardRef = useRef<HTMLDivElement>(null);
